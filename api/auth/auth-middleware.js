4fdc176e614a0b9f57f8fd19463cb12262707be3
MurrayWarnock <murray.warnock@gmail.com>
2021-08-17 15:32:37 -0600

    middleware restricted and only, all endpoints working, all CodeGrade tests passed

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,5 @@
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { JWT_SECRET } = require("../secrets/index"); // use this secret!
+const jwt = require('jsonwebtoken')
 const { findBy } = require('../users/users-model');
 
 
@@ -18,10 +19,24 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
-  next();
+    const token = req.headers.authorization;
+
+    if (token) {
+      // async function
+      jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) {
+          next({ status: 401, message: "Token invalid" });
+        } else {
+          req.decodedJwt = decoded;
+          next();
+        }
+      }); // old style node async callback, error first
+    } else {
+      next({ status: 401, message: "Token required" });
+    }
 };
 
-const only = role_name => (req, res, next) => {
+const only = (role) => (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
@@ -32,9 +47,12 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
- next();
+  if (req.decodedJwt.role_name === role) {
+    next();
+  } else {
+    next({ status: 403, message: "This is not for you" })
+  }
 };
-
   /*
     If the username in req.body does NOT exist in the database
     status 401
